d14727e975c075c19f0d84a4fe930b72fd4a881a
rat395 <rat395@ymail.com>
2017-04-16 23:12:44 -0700

    added header file for the table class

---
+++ cs133_assign/assign5/hash_table.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class hash_table{
+	public:
+		void insert(string k, int v);
+		int& find(string k);
+
+	private:
+		std::vector<int> table;
+};
