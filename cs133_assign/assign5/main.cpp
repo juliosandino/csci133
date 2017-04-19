9766438b76ecd18595257e4f138cb1b34d225429
rat395 <rat395@ymail.com>
2017-04-18 21:53:42 -0700

    reworked assignment 5

--- cs133_assign/assign5/main.cpp
+++ cs133_assign/assign5/main.cpp
@@ -1,33 +1,68 @@
-/*
-Created by Julio Sandino
-04/15/17
-*/
-
+#include <iostream>
 #include <vector>
+#include <string>
 #include <cstdint>
-#include "hash.cpp"
-#include <cmath>
+#include <math.h>
+#include <fstream>
+#include "boost_1_63_0/boost/math/distributions/chi_squared.hpp"
 
-using std::vector;
+using std::string;
 using std::cout;
 
+static int MOD = 65536;
+
+void reader();
+uint16_t str_length(string str);
+int hash_maker(uint16_t k);
+void tester();
+
 int main() {
+
+	tester();
 	
-	hash h1;
-	string test = "Hello";
-	vector<int> hashes;
-
-	cout << "Expected: " << test.length() % 65536 << "\n";
-	cout << "Result: " << h1.hash_length(test) << "\n";
-
-	cout << "Expected: " << (int) test.at(0) << "\n";
-	cout << "Result: " << h1.hash_firstChar(test) << "\n";
-
-	cout << "Expected: 500" << "\n";
-	cout << "Result: " << h1.hash_checksum(test) << "\n";
-
-	cout << "Expected: 310939249775" << "\n";
-	cout << "Result: " << h1.hash_remainder(test) << "\n";
-
 	return 0;
+}
+
+void reader() {
+	std::fstream file;
+	string line;
+	file.open("words.txt");
+
+	if (file.is_open())
+		while (getline(file, line))
+			cout << line << std::endl;		
+}
+
+uint16_t str_length(string str) {
+	return str.length() % MOD;
+}
+
+int hash_maker(uint16_t k) {
+	double A = 0.6180339887;
+	return MOD * fmod(k*A, 1);
+}
+
+void tester() {
+	std::vector<int> hashes (65536);
+	std::fstream file;
+	string line;
+	file.open("words.txt");
+
+	if (file.is_open())
+		while (getline(file, line))
+			hashes[hash_maker(str_length(line))]++;
+
+//	for (int i = 0; i < MOD; i++)
+//		if (hashes[i] > 0)
+//			cout << i << ": " << hashes[i] << "\n";
+	
+	float expected = 100000 / 65536;
+	float c2;
+
+	for (int i = 0; i < MOD; i++) {
+		c2 += pow(expected - hashes[i], 2) / expected;
+	}
+	
+	double c2d;
+
 }
