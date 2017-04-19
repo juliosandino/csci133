843b3acb3c1b8717de88e3e5beaa9fc65319970f
rat395 <rat395@ymail.com>
2017-04-18 22:44:49 -0700

    added multiplicative hasher function

--- cs133_assign/assign5/main.cpp
+++ cs133_assign/assign5/main.cpp
@@ -17,15 +17,18 @@ uint16_t str_length(string str);
 uint16_t first_char(string str);
 uint16_t checksum(string str);
 uint16_t hash_maker(uint16_t k);
+uint16_t multiplicative_hasher(string string);
 void str_length_tester();
 void first_char_tester();
 void checksum_tester();
+void multiplicative_tester();
 
 int main() {
 
 	str_length_tester();
 	first_char_tester();
 	checksum_tester();
+	multiplicative_tester();
 	
 	return 0;
 }
@@ -55,6 +58,15 @@ uint16_t checksum(string str) {
 			sum += (int)c;
 
 	return sum;
+}
+
+uint16_t multiplicative_hasher(string str) {
+	uint16_t hash = 0;
+	
+	for (char c: str)
+		hash = fmod(256* hash + A * c, 1);
+
+	return hash;
 }
 
 uint16_t hash_maker(uint16_t k) {
@@ -132,4 +144,28 @@ void checksum_tester() {
 	
 
 	cout << "Checksum: p = " << p << "\n";
+}
+
+void multiplicative_tester() {
+	std::vector<int> hashes (65536);
+	std::fstream file;
+	string line;
+	file.open("words.txt");
+
+	if (file.is_open())
+		while (getline(file, line))
+			hashes[multiplicative_hasher(line)]++;
+
+	float expected = 100000 / 65536;
+	float c2;
+
+	for (int i = 0; i < MOD; i++){
+		c2 += pow(expected - hashes[i], 2) / expected;
+	}
+
+	boost::math::chi_squared c2d(65535.0);
+	float p = boost::math::cdf(c2d, c2);
+	
+
+	cout << "Multiplicative: p = " << p << "\n";
 }
